0bcf4f0d3d3f360bc79c98a850a4333c4700c7cb
Sergey <sergeytan2609@mail.ru>
2026-09-09 19:46:03 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,10 @@
-// ВСТАВЬТЕ СЮДА конфигурацию вашего Firebase-проекта.
-// Firebase Console → Project settings → Your apps → Web app → SDK setup.
-export default {
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
   apiKey: "AIzaSyAe4czA1POr7_fwVnqXljD_5_X3rFTZIlg",
   authDomain: "b0n3n1.firebaseapp.com",
   projectId: "b0n3n1",
   storageBucket: "b0n3n1.firebasestorage.app",
   messagingSenderId: "42178037933",
-  appId: "1:42178037933:web:5336bf1f26603f94e85e24",
-  measurementId: "G-XE02N42FR7",
-  databaseURL: "https://b0n3n1-default-rtdb.europe-west1.firebasedatabase.app/"
+  appId: "1:42178037933:web:5336fb1f26603f94e85e24",
+  measurementId: "G-XE02N42FR7"
 };
