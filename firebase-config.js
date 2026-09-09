6d82656b5b3b5190a41927a56a79875c56e8bc72
Sergey <sergeytan2609@mail.ru>
2026-09-09 20:21:16 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,3 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAe4czA1POr7_fwVnqXljD_5_X3rFTZIlg",
   authDomain: "b0n3n1.firebaseapp.com",
@@ -6,6 +5,8 @@ const firebaseConfig = {
   storageBucket: "b0n3n1.firebasestorage.app",
   messagingSenderId: "42178037933",
   appId: "1:42178037933:web:5336fb1f26603f94e85e24",
-  measurementId: "G-XE02N42FR7"
+  measurementId: "G-XE02N42FR7",
+  databaseURL: "https://b0n3n1-default-rtdb.europe-west1.firebasedatabase.app/"
 };
+
 export default firebaseConfig;
